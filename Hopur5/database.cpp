38ad944e3210e2d367223c1342793426f8e5f9b0
Ladanvini <ladan.pourvatan@gmail.com>
2016-11-29 12:03:43 +0000

    trying to parse string array

--- Hopur5/database.cpp
+++ Hopur5/database.cpp
@@ -12,7 +12,7 @@ Database::Database(string dbFile)
     vector<string> strstr;
     ifstream fin;
 
-    fin.open("database.txt");
+    fin.open("/home/ladanvini/Documents/VLN1/VLN1-Hopur-5/Hopur5/database.txt");
     if(fin.is_open())
         cout << "fucking success!" << endl;
     else
@@ -28,15 +28,69 @@ Database::Database(string dbFile)
 //cout << str << endl;
         fin.getline(str, 255);
     }
-    for(int i=0; i< strstr.size(); i++)
-        cout << strstr.at(i) << endl;
-    fin.close();
- /*   while(!strstr.empty()){
-        string s = strstr.at(strstr.size()-1);
-        strstr.pop_back();
-        cout << s << endl;
+    string line = "";
+    string name;
+    char sex;
+    string birth;
+    int death;
+    string cont;
+    int ty;
+    for(int i=0; i<strstr.size(); i++){
+        line = strstr.at(i);
+        while(i<strstr.size() && strstr.at(i) != "#"){
+ //namw
+            int count = 0;
+            while(i<strstr.size() && line[count] != ':' && count < line.size()){
+                count ++;
+            }
+            count++;
+
+            while(i<strstr.size() && line[count] != '\n' && count < line.size()){
+
+                name = name + line[count];
+                count++;
+            }
+            cout << name << endl;
+//Sex
+            line = strstr.at(i);
+            i++;
+            count = 0;
+            while(i<strstr.size() && line[count] != ':' && count < line.size()){
+                count ++;
+            }
+            count++;
+
+            while(i<strstr.size() && line[count] != '\n' && count < line.size()){
+                sex = line[count];
+                count++;
+            }
+            cout << sex << endl;
+//Birth
+            line = strstr.at(i);
+            i++;
+            count = 0;
+
+            while(i<strstr.size() && line[count] != ':' && count < line.size()){
+                count ++;
+            }
+            count++;
+
+
+            while(i<strstr.size() && line[count] != '\n' && count < line.size()){
+                birth ="";
+                birth = birth + line[count];
+
+                count++;
+            }
+
+            cout << birth << endl;
+            line = strstr.at(i);
+            i++;
+        }
+
+
+
     }
-*/
 }
 /*Person Database::getPerson()
 {
