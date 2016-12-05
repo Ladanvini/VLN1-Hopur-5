9e775773a922a046865f531fb82e8444045e4a7c
smaring7 <sng16@hotmail.com>
2016-12-05 13:12:11 +0000

    sort computers

--- Hopur5/compservice.cpp
+++ Hopur5/compservice.cpp
@@ -51,12 +51,58 @@ vector<Comps> searchByName(string name);
 vector<Comps> searchByType(string type);
 vector<Comps> searchByBuilt(string contribution);
 // Sort
-vector<Comps> sortByName();
-vector<Comps> sortByType();
+vector<Comps> sortByName() {
+    vector<string> names;
+    vector<Comps> sorted;
+    for(unsigned int i = 0; i < computers.size(); i++)
+        names.push_back(computers.at(i).getName());
+
+    std::sort(names.begin(), names.end());
+
+    for(unsigned int i = 0; i < names.size(); i++) {
+        for(unsigned int j = 0; j < computers.size(); j++)
+            if(names.at(i) == computers.at(j).getName())
+                sorted.push_back(computers.at(j));
+    }
+
+    return sorted;
+}
+vector<Comps> sortByType() {
+    vector<string> types;
+    vector<Comps> sorted;
+    for(unsigned int i = 0; i < computers.size(); i++)
+        types.push_back(computers.at(i).getType());
+
+    std::sort(types.begin(), types.end());
+
+    for(unsigned int i = 0; i < types.size(); i++) {
+        for(unsigned int j = 0; j < computers.size(); j++)
+            if(types.at(i) == computers.at(j).getType())
+                sorted.push_back(computers.at(j));
+    }
+
+    return sorted;
+
+}
 
 
-vector<Comps> sortByBuilt();
+vector<Comps> sortByBuilt() {
+    vector<string> builtComps;
+    vector<Comps> sorted;
+    for(unsigned int i = 0; i < computers.size(); i++)
+        builtComps.push_back(computers.at(i).getBuilt());
 
+    std::sort(builtComps.begin(), builtComps.end());
+
+    for(unsigned int i = 0; i < builtComps.size(); i++) {
+        for(unsigned int j = 0; j < computers.size(); j++)
+            if(builtComps.at(i) == computers.at(j).getBuilt())
+                sorted.push_back(computers.at(j));
+    }
+
+    return sorted;
+
+}
 //DELETE
 string deleteComputers(string name, string type);
 // Edit
