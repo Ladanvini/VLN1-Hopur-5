c901c84542f893a1a0a43fa73082cdabe49afc65
Finnur Stefansson <Finnur Stefansson>
2016-12-07 14:14:25 +0000

    Pushing for Smári to see

--- Hopur5/main.cpp
+++ Hopur5/main.cpp
@@ -20,7 +20,7 @@ int main(int argc, char *argv[]) {
 
 
 //    string path = QCoreApplication::applicationDirPath().toStdString() + "/database.txt";
-    Database* db = new Database("/home/smari/Documents/Verklegt/VLN1-Hopur-5/create.sqlite");
+    Database* db = new Database("\create.sqlite");
     appservice* doStuff = new appservice(*db);
     CompService* cs = new CompService(*db);
     ConsoleUI ui(*doStuff, *cs);
