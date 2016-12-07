a4f397c2aeb4c5ea0a87822014df2c27499d43f4
Alexandra Geirsdóttir <alexandrawinther@Alexandras-MacBook-Air.local>
2016-12-07 12:41:48 +0000

    added functions for computer search menu, and mpore in consoleui

--- Hopur5/consoleui.cpp
+++ Hopur5/consoleui.cpp
@@ -337,3 +337,204 @@ void ConsoleUI::displayList() {
     cout << _process.showPeople(_process.getList());
 }
 
+
+
+
+
+//Comps**************************
+//*******************************
+//search
+void ConsoleUI::searchMenuComp() {
+    string input = "";
+    bool exitMenu = false;
+
+    string lowline = "--------------------------------------------------------------\n";
+
+    do {
+        cout << "Please enter one of these commands" << endl;
+        cout << "Name - Search by computer\'s name (Using correct capitalization of letters)" << endl;
+        cout << "ID - Search by computer\'s ID" << endl;
+        cout << "Type - Search by computer\'s type" << endl;
+        cout << "Built - Search by if computer has been built and then when" << endl;
+        cout << "Back - Go back to the main menu" << endl;
+
+        cout << lowline << endl;
+
+        input = inputHandling();
+
+        if(input == "name") {
+            cout << lowline;
+            cout << "Enter the name here: " << endl;
+            input = inputHandling();
+            cout << lowline;
+            cout << _compService.showComputers(_compService.searchByName(input));
+        }
+        else if(input == "unicorn"){
+            cout << lowline;
+            cout << unicorn();
+            cout << lowline;
+        }
+        else if(input == "id") {
+            cout << lowline;
+            cout << "Please enter the ID you want" << endl;
+            input = inputHandling();
+            cout << lowline;
+            int id;
+            id = stoi(input);
+            cout << _compService.showComputers(_compService.searchById(id));
+        }
+        else if(input == "built") {
+            cout << lowline;
+            cout << "Please enter yes or no if built or not" << endl;
+            input = inputHandling();
+            cout << lowline;
+            int ifbuilt;
+            ifbuilt = stoi(input);
+            cout << _compService.showComputers(_compService.searchByBuilt(ifbuilt));
+        }
+        else if(input == "type") {
+            cout << lowline;
+            cout << "Enter the type please" << endl;
+            input = inputHandling();
+            cout << lowline;
+            cout << _compService.showComputers(_compService.searchByType(input));
+        }
+        else if(input == "back") {
+            cout << lowline;
+            cout << "Thank you, taking you back to the main menu" << endl;
+            cout << lowline;
+            exitMenu = true;
+        }
+        else {
+            cout << lowline;
+            cout << "Wrong Input! Please try again." << endl;
+            cout << lowline;
+        }
+    }while(!exitMenu);
+}
+
+//sortMenu
+/*void ConsoleUI::sortMenuComp() {
+    string lowline = "--------------------------------------------------------------\n";
+    string input = "";
+    bool exitMenu = false;
+
+    do {
+        cout << "Please enter one of these commands" << endl;
+        cout << "Name - Sort by computer's name" << endl;
+        cout << "ID - Sort by computer's ID" << endl;
+        cout << "Type - Sort by computer's type" << endl;
+        cout << "Built - Sort by if computer was built" << endl;
+        cout << "Back - Back to main menu" << endl;
+
+        cout << lowline;
+
+        input = inputHandling();
+
+        if(input == "name") {
+            cout << lowline;
+            cout << "The list sorted alphabetically: " << endl;
+            cout << lowline;
+            cout << _compService.showComputers(_compService.sortByName());
+        }
+/*        else if(input == "id") {
+            cout << lowline;
+            cout << "The list sorted by ID number: " << endl;
+            cout << lowline;
+            cout << _compService.showComputers(_compService.sortByID());
+        }
+        else if(input == "type") {
+            cout << lowline;
+            cout << "The list sorted by type: " << endl;
+            input = inputHandling();
+            cout << lowline;
+            cout << _compService.showComputers(_compService.sortByType());
+        }
+        else if(input == "built") {
+            cout << lowline;
+            cout << "Sorted from eldest to youngest" << endl;
+            cout << lowline;
+            cout << _compService.showComputers(_compService.sortByBuilt());
+        }
+        else if(input == "back"){
+            cout << lowline;
+            cout << "Thank you, taking you back to the main menu" << endl;
+            cout << lowline;
+            exitMenu = true;
+        }
+        else {
+            cout << lowline;
+            cout << "wrong input! Try again" << endl;
+            cout << lowline;
+        }
+
+    }while(!exitMenu);
+}
+
+void ConsoleUI::runUIComp() {
+    string input = " ";
+    bool exitUI = false;
+    string lowline = "--------------------------------------------------------------\n";
+    cout << "*******************FAMOUS COMPUTERS*********************" << endl;
+
+    do {
+        cout << lowline << endl;
+        cout << "Please input one of these commands: " << endl;
+        cout << "Display - Displays the list of famous computers" << endl;
+        cout << "Add - Adds a computer to the list" << endl;
+        cout << "Search - Searches for a computer in the list" << endl;
+        cout << "Sort - Displays the list sorted order from options" << endl;
+        cout << "Delete - Deletes a computer from the list" << endl;
+        cout << "Unicorn - A unicorn" << endl;
+//BACK MENU INSTEAD OF EXIT?
+        cout << "Exit - End the programs run" << endl;
+
+        input = inputHandling();
+
+        if(input == "add") {
+            cout << lowline;
+            createMenu();
+        }
+        else if(input == "unicorn"){
+            cout << lowline;
+            cout << unicorn();
+            cout << lowline;
+        }
+        else if(input == "search") {
+            cout << lowline;
+            searchMenu();
+        }
+        else if(input == "sort") {
+            cout << lowline;
+            sortMenu();
+        }
+        else if(input == "display") {
+            cout << lowline;
+            displayList();
+        }
+        else if(input == "delete") {
+            string name;
+            string type;
+            cout << lowline;
+            cout << "Enter the computer's name: \n";
+            getline(cin, name);
+            cout << "Enter the computer's type: \n";
+            getline(cin, type);
+            cout << _compService.deleteComputers(name, type) << endl;
+            cout << lowline << endl;
+        }
+        else if(input == "exit") {
+            cout << lowline;
+            cout << "Thank you, have a nice day!" << endl;
+            cout << lowline;
+            exitUI = true;
+        }
+        else {
+            cout << lowline;
+            cout << "Wrong input!" << endl;
+            cout << lowline;
+        }
+
+    }while(!exitUI);
+}
+*/
--- Hopur5/consoleui.h
+++ Hopur5/consoleui.h
@@ -2,6 +2,7 @@
 #define CONSOLEUI_H
 
 #include "appservice.h"
+#include "compservice.h"
 
 #include <string>
 
@@ -10,6 +11,7 @@ using namespace std;
 class ConsoleUI {
 private:
     Process _process;
+    CompService _compService;
 public:
     ConsoleUI();
     ConsoleUI(Process p);
@@ -21,6 +23,12 @@ public:
     void runUI();
     string unicorn();
     void displayList();
+
+    void searchMenuComp();
+    void sortMenuComp();
+    void createMenuComp();
+    void runUIComp();
+    void displayListComp();
 };
 
 #endif // CONSOLEUI_H
