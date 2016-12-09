b4f00e66fc38ce8f70ed04ec1a0256ef8234dccf
Alexandra Geirsdóttir <alexandrawinther@Alexandras-MacBook-Air.local>
2016-12-09 16:41:27 +0000

    fixed tictactoe

--- Hopur5/consoleui.cpp
+++ Hopur5/consoleui.cpp
@@ -1158,6 +1158,8 @@ void ConsoleUI::runUI() {
         }
         else if(input == "tictactoe") {
             _ticTacToe.ticTacToeMain();
+            cin.clear();
+            cin.ignore(INT_MAX, '\n');
         }
         else {
             cout << lowline;
