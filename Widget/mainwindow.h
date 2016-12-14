879a5e9d3293b67f833ff7139f67333ca01350e7
Finnur Stefansson <Finnur Stefansson>
2016-12-14 16:27:31 +0000

    Fixed the search bars

--- Widget/mainwindow.h
+++ Widget/mainwindow.h
@@ -14,7 +14,7 @@
 #include "funmenu.h"
 
 
-static QString _dbpath = "/home/smari/Documents/Verklegt/VLN1-Hopur-5/create.sqlite";
+static QString _dbpath = "C:\VLN1-Hopur-5\create.sqlite";
 
 static appservice _ps = appservice(_dbpath);
 static CompService _cs = CompService(_dbpath);
